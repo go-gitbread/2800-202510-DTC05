80b64101c30b5604077f39be3bd8fa5cfb5a09f1
Roger Vanderheyden <rogecheflife@gmail.com>
2025-05-05 19:12:26 -0700

    Add empty routine list & "Create Routine" button

--- server.js
+++ server.js
@@ -67,4 +67,6 @@ app.get('/logout', (req, res) => {
   });
 });
 
+app.get('/routines', (req, res) => res.render('routines'));
+
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
