f9196435a22e0702475a3e17f7e70f56bc0d70de
klockwork3 <klockwork3@gmail.com>
2025-05-14 07:58:27 -0700

    Add error handling for incorrect password when logging in

--- server.js
+++ server.js
@@ -86,6 +86,10 @@ app.post('/login', async (req, res) => {
     });
   }  
 
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.render('login', { error: 'Incorrect password. Try again.' });
+  }
 
   req.session.userId = user._id;
   req.session.userEmail = user.email;
